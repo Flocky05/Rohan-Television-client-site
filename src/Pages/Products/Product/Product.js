b9a96644fe73f19b068a4d9515bf31b546d0ec3a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-11-28 20:37:39 +0600

    added modal part

--- src/Pages/Products/Product/Product.js
+++ src/Pages/Products/Product/Product.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../Contexs/AuthProvider/AuthProvider';
 
 const Product = ({ product }) => {
+    const { user } = useContext(AuthContext);
     const { name, _id, image,
         location, resalePrice, originalPrice,
         yearsOfUse, SellerName } = product;
@@ -16,7 +18,49 @@ const Product = ({ product }) => {
                     <p>Year of use:{yearsOfUse}</p>
                     <p>SellerName:{SellerName}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy</button>
+                        <div className="bg-gradient-to-r cursor-pointer from-sky-500 to-indigo-500 text-white px-4 py-2 rounded mr-2">
+                            <label htmlFor="my-modal-3" className='cursor-pointer'
+                            >Book Now</label>
+
+                            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                            <div className="modal">
+                                <div className="modal-box relative text-black">
+                                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                    <form className="card-body">
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Name</span>
+                                            </label>
+                                            <input type="text" name='name' disabled defaultValue={user?.displayName} className="input input-bordered" />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Email</span>
+                                            </label>
+                                            <input type="text" name='email' disabled defaultValue={user?.email} className="input input-bordered" />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Meeting location</span>
+                                            </label>
+                                            <input type="text" name='location' placeholder="location" className="input input-bordered" />
+                                        </div>
+
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Phone Number</span>
+                                            </label>
+                                            <input type="text" name='phoneNumber' placeholder="Phone Number" className="input input-bordered" />
+
+                                        </div>
+                                        <div className="form-control mt-6">
+                                            <input className="bg-gradient-to-r from-sky-500 to-indigo-500 text-white px-4 py-2 rounded mr-2 w-full" type="submit" value="Submit" />
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
