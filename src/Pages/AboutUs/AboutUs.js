0265c01f4b3d40eff0b1d407eea416459e80fdf8
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-29 22:54:32 +0600

    added joining team

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,42 +1,83 @@
-import React from 'react';
+import React from "react";
+import JoniningTeam from "./JoiningTeam/JoniningTeam";
 
 const AboutUs = () => {
-    return (
-        <div>
-            <div className='m-3'>
-                <h2 className='text-2xl font-semibold'>About Rohan Television.com, The Largest television Marketplace in Bangladesh!</h2>
-                <p className='mb-8'>
-                    Rohan Television is a platform on which you can buy and sell televsion! We help people buy and sell televsion. With more than 50 categories our solutions are built to be safe, smart, and convenient for our customers.
-                </p>
-            </div>
-            <div className='m-3'>
-                <h2 className='text-2xl font-semibold'>Buy, Sell, Rent, or Find Jobs in Bangladesh</h2>
+  return (
+    <div>
+      <div className="m-3">
+        <h2 className="text-2xl font-semibold">
+          About Rohan Television.com, The Largest television Marketplace in
+          Bangladesh!
+        </h2>
+        <p className="mb-8">
+          Rohan Television is a platform on which you can buy and sell
+          televsion! We help people buy and sell televsion. With more than 50
+          categories our solutions are built to be safe, smart, and convenient
+          for our customers.
+        </p>
+      </div>
+      <div className="m-3">
+        <h2 className="text-2xl font-semibold">
+          Buy, Sell, Rent, or Find Jobs in Bangladesh
+        </h2>
 
-                <p>
-                    Every month, hundreds of millions of people use Rohan Television to find and sell televison ads. To sell new items or sell used items quickly, Rohan Television offers Ad Promotion features.
-                </p>
-                <p>
-                    Rohan Television.com has an extensive collection of new and used goods, making it easier for users to quickly buy new items or buy used items at their desired location. To buy online, users easily can reach their desired products through filtering options.
-                </p>
-                <p>
-                    Doorstep Delivery is the e-commerce service of Rohan Television.com, serving necessary appliances with a 100% guarantee of safety and saving valuable time for buyers and sellers in online shopping.
-                </p>
-                <p>
-                    For businesses, Rohan Television.com has introduced the ‘Membership’ service, which helps them expand their businesses online. Members will have their virtual shop with a Rohan Television URL with free promotions of their goods. With different membership categories, businesses can flourish as verified members and authorized dealers.
-                </p>
-                <p className='mb-8'>
-                    With millions of unique monthly visitors, thousands of interested buyers, and thousands of active dealers on our platform, Rohan Television.com is the Largest Marketplace in Bangladesh.
-                </p>
-            </div>
-            <div className='m-3'>
-                <h2 className='text-2xl font-semibold'>Benefits of Trading at Rohan Television.com</h2>
-                <p>At Rohan Television, we make it so easy to connect people to buy, sell or rent goods and services as other classified sites.</p>
-                <p>Fast & Easy Experience: Navigated buying and selling experience in Bangladesh which is simpler, faster, and easier. Shop and sell on the go and get your desired products in just a few clicks.</p>
-                <p> Post Ads Free: As a free classified website, we offer free ad posting features in many categories for the convenience of the users based on their locations.</p>
-                <p> Safe & Secure Shopping: We have listed our verified members and authorized dealers to create a safe shopping experience for our users.</p>
-            </div>
-        </div>
-    );
+        <p>
+          Every month, hundreds of millions of people use Rohan Television to
+          find and sell televison ads. To sell new items or sell used items
+          quickly, Rohan Television offers Ad Promotion features.
+        </p>
+        <p>
+          Rohan Television.com has an extensive collection of new and used
+          goods, making it easier for users to quickly buy new items or buy used
+          items at their desired location. To buy online, users easily can reach
+          their desired products through filtering options.
+        </p>
+        <p>
+          Doorstep Delivery is the e-commerce service of Rohan Television.com,
+          serving necessary appliances with a 100% guarantee of safety and
+          saving valuable time for buyers and sellers in online shopping.
+        </p>
+        <p>
+          For businesses, Rohan Television.com has introduced the ‘Membership’
+          service, which helps them expand their businesses online. Members will
+          have their virtual shop with a Rohan Television URL with free
+          promotions of their goods. With different membership categories,
+          businesses can flourish as verified members and authorized dealers.
+        </p>
+        <p className="mb-8">
+          With millions of unique monthly visitors, thousands of interested
+          buyers, and thousands of active dealers on our platform, Rohan
+          Television.com is the Largest Marketplace in Bangladesh.
+        </p>
+      </div>
+      <div className="m-3">
+        <h2 className="text-2xl font-semibold">
+          Benefits of Trading at Rohan Television.com
+        </h2>
+        <p>
+          At Rohan Television, we make it so easy to connect people to buy, sell
+          or rent goods and services as other classified sites.
+        </p>
+        <p>
+          Fast & Easy Experience: Navigated buying and selling experience in
+          Bangladesh which is simpler, faster, and easier. Shop and sell on the
+          go and get your desired products in just a few clicks.
+        </p>
+        <p>
+          {" "}
+          Post Ads Free: As a free classified website, we offer free ad posting
+          features in many categories for the convenience of the users based on
+          their locations.
+        </p>
+        <p>
+          {" "}
+          Safe & Secure Shopping: We have listed our verified members and
+          authorized dealers to create a safe shopping experience for our users.
+        </p>
+      </div>
+      <JoniningTeam></JoniningTeam>
+    </div>
+  );
 };
 
 export default AboutUs;
